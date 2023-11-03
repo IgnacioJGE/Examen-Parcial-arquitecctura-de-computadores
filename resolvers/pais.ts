98ea1de6c8ed1d1b193d6806ea2b67ca0c1d8aa8
Ignacio García <ijgarciaest@gmail.com>
2023-11-03 16:40:13 +0100

    modified:   resolvers/pais.ts

--- resolvers/pais.ts
+++ resolvers/pais.ts
@@ -9,7 +9,7 @@ export const getPais = async (ISO: string): Promise<string> => {
     const URL = "https://restcountries.com/v3.1/alpha/";
     const URL_final= `${URL}${ISO}`
     const data = await fetch(URL_final);
-  
+  console.log("Estoy entrando en esta fncion")
     if (data.status !== 200) {
       throw new Error
 
